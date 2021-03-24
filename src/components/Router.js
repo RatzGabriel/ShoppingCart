1ddc5e1618e3f900139f6ac2c8f2bae54b8c7a63
gabriel <gabrielratz84@gmai.com>
2021-03-24 13:48:23 +0100

    fist

--- src/components/Router.js
+++ src/components/Router.js
@@ -8,18 +8,132 @@ import Cart from "./Cart";
 
 const ProductsContext = createContext();
 
+const data = [
+  {
+    title: "Item 1",
+    src: "/images/1.jpg",
+    src2: "/images/2.jpg",
+    src3: "/images/3.jpg",
+    src4: "/images/4.jpg",
+    price: "999",
+    featured: true,
+    id: 0,
+    amount: 0,
+  },
+  {
+    title: "Item 2",
+    src: "/images/2.jpg",
+    src2: "/images/2.jpg",
+    price: "999",
+    featured: true,
+    id: 1,
+    amount: 0,
+  },
+  {
+    title: "Item 3",
+    src: "/images/3.jpg",
+    src2: "/images/2.jpg",
+    price: "999",
+    featured: true,
+    id: 2,
+    amount: 0,
+  },
+  {
+    title: "Item 3",
+    src: "/images/4.jpg",
+    src2: "/images/2.jpg",
+    price: "999",
+    featured: false,
+    id: 3,
+    amount: 0,
+  },
+  {
+    title: "Item 4",
+    src: "/images/4.jpg",
+    src2: "/images/2.jpg",
+    price: "999",
+    featured: false,
+    id: 4,
+    amount: 0,
+  },
+  {
+    title: "Item 5",
+    src: "/images/4.jpg",
+    src2: "/images/2.jpg",
+    price: "999",
+    featured: false,
+    id: 5,
+    amount: 0,
+  },
+  {
+    title: "Item 6",
+    src: "/images/4.jpg",
+    src2: "/images/2.jpg",
+    price: "999",
+    featured: false,
+    id: 6,
+    amount: 0,
+  },
+  {
+    title: "Item 7",
+    src: "/images/4.jpg",
+    src2: "/images/2.jpg",
+    price: "999",
+    featured: false,
+    id: 7,
+    amount: 0,
+  },
+  {
+    title: "Item 8",
+    src: "/images/4.jpg",
+    src2: "/images/2.jpg",
+    price: "999",
+    featured: false,
+    id: 8,
+    amount: 0,
+  },
+  {
+    title: "Item 9",
+    src: "/images/4.jpg",
+    src2: "/images/2.jpg",
+    price: "999",
+    featured: false,
+    id: 9,
+    amount: 0,
+  },
+  {
+    title: "Item 10",
+    src: "/images/4.jpg",
+    src2: "/images/2.jpg",
+    price: "999",
+    featured: false,
+    id: 10,
+    amount: 0,
+  },
+  {
+    title: "Item 11",
+    src: "/images/4.jpg",
+    src2: "/images/2.jpg",
+    price: "999",
+    featured: false,
+    id: 11,
+    amount: 0,
+  },
+];
+
 const Router = () => {
   const [items, setItems] = useState(null);
   const [total, setTotal] = useState(0);
 
   useEffect(() => {
-    const data = fetch("http://localhost:8000/items")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setItems(data);
-      });
+    // const data = fetch("data/db.json")
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((data) => {
+    //     setItems(data);
+    //   });
+    setItems(data);
   }, []);
 
   const add = (item) => {
